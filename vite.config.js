5971e5b381339c8b072f215743b0f7185f24e53f
Laura Sofia Heimann <hello@laura.media>
2023-05-30 18:10:15 +0200

    Auto Deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'development' ? '/' : '/WebTrax/',
   plugins: [vue()],
   resolve: {
     alias: {
