fea45140481785bacd4c2ab1b997706cecc176b0
Laura Sofia Heimann <hello@laura.media>
2023-05-30 18:13:42 +0200

    Fixed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'development' ? '/' : '/WebTrax/',
+  base: '/WebTrax/',
   plugins: [vue()],
   resolve: {
     alias: {
