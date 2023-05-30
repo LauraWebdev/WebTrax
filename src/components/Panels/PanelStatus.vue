7ac0e4857d9b8fa99063eac8332528e5bc240dd1
Laura Sofia Heimann <hello@laura.media>
2023-05-30 18:28:00 +0200

    Fixed Status Bar

--- src/components/Panels/PanelStatus.vue
+++ src/components/Panels/PanelStatus.vue
@@ -1,6 +1,11 @@
 <template>
     <section class="panel-status">
-        Selection Info, Blocks Count, Length
+        <span class="copyright">Created by <a href="https://laura.media" target="_blank">Laura Sofia Heimann</a></span>
+
+        <nav>
+            <a href="https://github.com/LauraWebdev/WebTrax" target="_blank">Github</a>
+            <a href="https://habbo.com" target="_blank">Habbo</a>
+        </nav>
     </section>
 </template>
 
@@ -9,5 +14,38 @@
 
 <style lang="scss" scoped>
 .panel-status {
+    display: grid;
+    grid-template-columns: 1fr auto;
+
+    & .copyright {
+        padding: 15px;
+
+        &:not(a) {
+            color: rgba(255,255,255,0.4);
+        }
+        & a {
+            color: #fff;
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+
+    & nav {
+        display: flex;
+        padding: 15px;
+        gap: 15px;
+
+        & a {
+            color: #fff;
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
 }
 </style>
